450d8393da96ea22d75c7b12d38cf703a2a5d953
jesterlink <sdi0700128@di.uoa.gr>
2016-06-10 14:11:38 +0000

    Add files via upload

--- modules/auth/newuser.php
+++ modules/auth/newuser.php
@@ -41,6 +41,7 @@
 include '../../include/baseTheme.php';
 include '../../include/sendMail.inc.php';
 include 'auth.inc.php';
+
 $nameTools = $langUserDetails;
 // Main body
 $navigation[] = array("url"=>"registration.php", "name"=> $langNewUser);
@@ -58,7 +59,23 @@ $lang = langname_to_code($language);
 // display form
 if (!isset($submit)) {
 	// Main body
-	@$tool_content .= "<form action='$_SERVER[PHP_SELF]' method='post'>
+	if(!empty($prenom_form)){
+		$prenom_formEsc = esc($prenom_form);
+	}
+	if(!empty($nom_form)){
+		$nom_formEsc = esc($nom_form);
+	}
+	if(!empty($uname)){
+		$unameEsc = esc($uname);
+	}
+	if(!empty($email)){
+		$emailEsc = esc($email);
+	}
+	if(!empty($am)){
+		$amEsc= esc($am);
+	}
+
+	@$tool_content .= "<form action=".esc($_SERVER['PHP_SELF'])." method='post'>
 	<table width='99%' style='border: 1px solid #edecdf;'>
 	<thead>
 	<tr>
@@ -67,15 +84,15 @@ if (!isset($submit)) {
 	<thead>
 	<tr>
 	<th class='left' width='220'>$langName</th>
-	<td colspan='2'><input type='text' name='prenom_form' value='$prenom_form' class='FormData_InputText' />&nbsp;&nbsp;<small>(*)</small></td>
+	<td colspan='2'><input type='text' name='prenom_form' value='$prenom_formEsc' class='FormData_InputText' />&nbsp;&nbsp;<small>(*)</small></td>
 	</tr>
 	<tr>
 	<th class='left'>$langSurname</th>
-	<td colspan='2'><input type='text' name='nom_form' value='$nom_form' class='FormData_InputText' />&nbsp;&nbsp;<small>(*)</small></td>
+	<td colspan='2'><input type='text' name='nom_form' value='$nom_formEsc' class='FormData_InputText' />&nbsp;&nbsp;<small>(*)</small></td>
 	</tr>
 	<tr>
 	<th class='left'>$langUsername</th>
-	<td colspan='2'><input type='text' name='uname' value='$uname' size='20' maxlength='20' class='FormData_InputText' />&nbsp;&nbsp;<small>(*) $langUserNotice</small></td>
+	<td colspan='2'><input type='text' name='uname' value='$unameEsc' size='20' maxlength='20' class='FormData_InputText' />&nbsp;&nbsp;<small>(*) $langUserNotice</small></td>
 	</tr>
 	<tr>
 	<th class='left'>$langPass</th>
@@ -87,12 +104,12 @@ if (!isset($submit)) {
 	</tr>
 	<tr>
 	<th class='left'>$langEmail</th>
-	<td valign='top'><input type='text' name='email' value='$email' class='FormData_InputText' /></td>
+	<td valign='top'><input type='text' name='email' value='$emailEsc' class='FormData_InputText' /></td>
 	<td><small>$langEmailNotice</small></td>
 	</tr>
 	<tr>
 	<th class='left'>$langAm</th>
-	<td colspan='2' valign='top'><input type='text' name='am' value='$am' class='FormData_InputText' /></td>
+	<td colspan='2' valign='top'><input type='text' name='am' value='$amEsc' class='FormData_InputText' /></td>
 	</tr>
 	<tr>
 	<th class='left'>$langFaculty</th>
@@ -146,7 +163,7 @@ if (!isset($submit)) {
 	if (!empty($email) and !email_seems_valid($email)) {
 		$registration_errors[] = $langEmailWrong;
 	}
-	$auth_method_settings = get_auth_settings($auth);
+	$auth_method_settings = get_auth_settings(escapeSimple($auth));
 	if (!empty($auth_method_settings) and $auth != 1) {
 		$password = $auth_method_settings['auth_name'];
 	} else {
@@ -200,7 +217,7 @@ if (!isset($submit)) {
 	$q1 = "INSERT INTO `$mysqlMainDb`.user
 	(user_id, nom, prenom, username, password, email, statut, department, am, registered_at, expires_at, lang)
 	VALUES ('NULL', '$nom_form', '$prenom_form', '$uname', '$password_encrypted', '$email','5',
-		'$department','$am',".$registered_at.",".$expires_at.",'$lang')";
+		'".$department."','$am',".$registered_at.",".$expires_at.",'$lang')";
 	$inscr_user = mysql_query($q1);
 	$last_id = mysql_insert_id();
 	$result=mysql_query("SELECT user_id, nom, prenom FROM `$mysqlMainDb`.user WHERE user_id='$last_id'");
@@ -231,7 +248,8 @@ if (!isset($submit)) {
 		foreach ($registration_errors as $error) {
 			$tool_content .= "<p>$error</p>";
 		}
-		$tool_content .= "<p><a href='$_SERVER[PHP_SELF]?prenom_form=$_POST[prenom_form]&nom_form=$_POST[nom_form]&uname=$_POST[uname]&email=$_POST[email]&am=$_POST[am]'>$langAgain</a></p>" .
+
+		$tool_content .= "<p><a href='".esc($_SERVER['PHP_SELF'])."?prenom_form=".esc($_POST['prenom_form'])."&nom_form=".esc($_POST['nom_form'])."&uname=".esc($_POST['uname'])."&email=".esc($_POST['email'])."&am=".esc($_POST['am'])."'>$langAgain</a></p>" .
 					"</td></tr></tbody></table><br /><br />";
 	}
 
