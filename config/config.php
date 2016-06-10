4cc75cc43de27001b0081e92837dbe9ce9b5bde8
Sakellari Elisavet <esakellari88@gmail.com>
2016-06-10 13:46:16 +0300

    openeclass

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+/* ========================================================
+ * OpeneClass 2.2 configuration file
+ * Automatically created by install on 2016-06-07 15:46
+ * ======================================================== */
+
+$urlServer = "http://localhost/openeclass/";
+$urlAppend = "/openeclass";
+$webDir    = "/opt/lampp/htdocs/openeclass/" ;
+
+$mysqlServer = "localhost";
+$mysqlUser = "root";
+$mysqlPassword = "";
+$mysqlMainDb = "eclass";
+$phpMyAdminURL = "../admin/mysql/";
+$phpSysInfoURL = "../admin/sysinfo/";
+$emailAdministrator = "you@example.com";
+$administratorName = "Διαχειριστής";
+$administratorSurname = "Πλατφόρμας";
+$siteName = "Open eClass";
+
+$telephone = "+30 2xx xxxx xxx";
+$fax = "";
+$emailhelpdesk = "";
+
+$language = "greek";
+
+$Institution = "Ακαδημαϊκό Διαδίκτυο GUNet ";
+$InstitutionUrl = "http://www.gunet.gr/";
+$postaddress = "";
+
+$have_latex = FALSE;
+$close_user_registration = FALSE;
+
+$persoIsActive = TRUE;
+$durationAccount = "126144000";
+
+define("UTF8", true);
+
+
+$encryptedPasswd = true;
